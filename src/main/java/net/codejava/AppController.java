fbfecc2f55e8409c412f5823750e9b4da71dc9e3
gilkom <gilek06@op.pl>
2020-05-05 19:09:15 +0200

    adding controller and main class

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppController {
+	@Autowired
+	private SalesDAO dao;
+
+}
--- src/main/java/net/codejava/SalesDAO.java
+++ src/main/java/net/codejava/SalesDAO.java
@@ -1,5 +1,7 @@
 package net.codejava;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class SalesDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	public List<Sale> list(){
+		return null;
+	}
+	
+	public void save(Sale sale) {
+	}
+	
+	public Sale get(int id) {
+		return null;
+	}
+	
+	public void update(Sale sale) {
+	}
+	
+	public void delete(int id) {
 }
